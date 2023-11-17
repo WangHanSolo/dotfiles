3051ab439ce0d0ac1a3954b39ec2f999161f7bbf
WangHanSolo <hwang638@wisc.edu>
2023-11-17 17:34:34 -0500

    moving to neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+
+-- IMPORTS
+require('vars')
+require('opts')
+require('keys')
+require('plugin')
--- .zshrc
+++ .zshrc
@@ -116,6 +116,7 @@ source $ZSH/oh-my-zsh.sh
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
+alias vim="nvim"
 PATH=/home/han/.local/bin:$PATH
 GTK_DEBUG=interactive
 export GTK_DEBUG=interactive
