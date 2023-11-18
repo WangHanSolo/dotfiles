c9b03ce96cea7d317ff61db9441afeae0b99b65e
WangHanSolo <hwang638@wisc.edu>
2023-11-18 18:18:43 -0500

    install pylsp by default

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -31,7 +31,9 @@ return {
             gopls = {},
             ruff_lsp = {},
             vimls = {},
-            yamlls = {}
+            yamlls = {},
+            black = {},
+            pylsp = {},
         },
         -- you can do any additional lsp server setup here
         -- return true if you don"t want this server to be setup with lspconfig
